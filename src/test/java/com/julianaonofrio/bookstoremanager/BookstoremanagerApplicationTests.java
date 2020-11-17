f557136e624d76befc461f7b2c7bf024688ab3ab
JulianaOnofrio <julianaonofrio@hotmail.com>
2020-11-17 01:14:33 -0300

    First commit: project structure created

---
+++ src/test/java/com/julianaonofrio/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianaonofrio.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
