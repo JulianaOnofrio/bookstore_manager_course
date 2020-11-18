f275831fffac318b0418c97b0e1d316c724f5879
JulianaOnofrio <julianaonofrio@hotmail.com>
2020-11-18 00:16:39 -0300

    Developed project entities

---
+++ src/main/java/com/julianaonofrio/bookstoremanager/entity/Author.java
@@ -0,0 +1,30 @@
+package com.julianaonofrio.bookstoremanager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
---
+++ src/main/java/com/julianaonofrio/bookstoremanager/entity/Book.java
@@ -0,0 +1,45 @@
+package com.julianaonofrio.bookstoremanager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer pages;
+
+    @Column(nullable = false)
+    private Integer chapters;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(name = "publisher_name", nullable = false,unique = true)
+    private String publisherName;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "author_id")
+    private Author author;
+}
